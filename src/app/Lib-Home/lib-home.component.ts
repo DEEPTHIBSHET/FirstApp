434b2c0668f130f3d07b463479f01eeafc9664e1
Deepthi B Shet <DB00557581@techmahindra.com>
2018-08-08 16:52:06 +0530

    Added my Project first time

---
+++ src/app/Lib-Home/lib-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lib-home',
+  templateUrl: './lib-home.component.html',
+  styleUrls: ['./lib-home.component.css']
+})
+export class LibHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import { MatIconModule } from "@angular/material/icon";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LibHomeComponent } from './lib-home/lib-home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LibHomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    MatCheckboxModule,
+                                                                MatIconModule,
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
